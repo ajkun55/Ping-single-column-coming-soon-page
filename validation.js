357db6690b666ca22b08e55bb87d1bd804303f9d
ajkun55 <81357643+ajkun55@users.noreply.github.com>
2024-04-20 18:30:30 +0800

    Add files via upload

--- validation.js
+++ validation.js
@@ -4,26 +4,25 @@
     
     if (email.length == 0) {
       document.getElementById("error").style.setProperty('display', 'block');
+      document.getElementById("email").style.setProperty('border-color', 'red');
     }
     
     if (!email.includes('@')) {
       document.getElementById("error").style["display"]="block";
+      document.getElementById("email").style.setProperty('border-color', 'red');
     }    
-    var emailHost = email.substr((-1)*(email.length - email.indexOf('@') - 1));
-    
-    var allowedDomains = ["gmail.com","hotmail.com","yahoo.com"];
-    
-    var inAllowed = false;
-    for(i=0;i<allowedDomains.length;i++){
-      if (allowedDomains[i] == emailHost) {
-        inAllowed = true;
-      }
+  
+    function validateEmail(sumbittedemail) {
+      var re = /\S+@\S+\.\S+/;
+      return re.test(submittedemail);
     }
     
-    if (!inAllowed) {
+    if(!validateEmail(email)){
       document.getElementById("error").style["display"]="block";
+      document.getElementById("email").style.setProperty('border-color', 'red');
     }else { //submit form here
       document.getElementById("error").style.setProperty('display', 'none');
+      document.getElementById("email").style.setProperty('border-color', 'none');
       $('#emailForm').submit();
     }    
     }
